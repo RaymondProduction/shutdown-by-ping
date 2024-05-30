23d60ae96dd50bb76915add3f9d5ec7fb17220e3
Raymond Perminov <raymond@brizoit.com>
2024-05-30 17:19:57 +0300

    Added tray implementation

--- main.go
+++ main.go
@@ -2,24 +2,87 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 	"time"
+
+	"github.com/getlantern/systray"
 )
 
 const (
-	routerIP = "192.168.0.1"
+	routerIP = "127.0.0.1"
+)
+
+var (
+	pingTicker *time.Ticker
+	stopChan   chan bool
 )
 
 func main() {
-	for {
-		if !pingRouter(routerIP) {
-			fmt.Println("Router not reachable. Shutting down the system...")
-			shutdownSystem()
-		} else {
-			fmt.Println("Router reachable")
+	systray.Run(onReady, onExit)
+}
+
+func onReady() {
+	systray.SetIcon(getIcon("icon.ico")) // replace with your icon file
+	systray.SetTitle("Ping Router")
+	systray.SetTooltip("Ping Router Service")
+
+	startItem := systray.AddMenuItem("Start Ping", "Start pinging the router")
+	stopItem := systray.AddMenuItem("Stop Ping", "Stop pinging the router")
+	quitItem := systray.AddMenuItem("Quit", "Quit the application")
+
+	stopItem.Disable()
+
+	go func() {
+		for {
+			select {
+			case <-startItem.ClickedCh:
+				startPinging()
+				startItem.Disable()
+				stopItem.Enable()
+			case <-stopItem.ClickedCh:
+				stopPinging()
+				startItem.Enable()
+				stopItem.Disable()
+			case <-quitItem.ClickedCh:
+				systray.Quit()
+				return
+			}
 		}
-		time.Sleep(1 * time.Minute)
+	}()
+}
+
+func onExit() {
+	// Cleanup here
+	stopPinging()
+}
+
+func startPinging() {
+	stopChan = make(chan bool)
+	pingTicker = time.NewTicker(1 * time.Minute)
+
+	go func() {
+		for {
+			select {
+			case <-pingTicker.C:
+				if !pingRouter(routerIP) {
+					fmt.Println("Router not reachable. Shutting down the system...")
+					shutdownSystem()
+				} else {
+					fmt.Println("Router reachable")
+				}
+			case <-stopChan:
+				pingTicker.Stop()
+				return
+			}
+		}
+	}()
+}
+
+func stopPinging() {
+	if stopChan != nil {
+		stopChan <- true
 	}
 }
 
@@ -42,4 +105,13 @@ func shutdownSystem() {
 	if err != nil {
 		fmt.Printf("Error shutting down the system: %v\n", err)
 	}
+}
+
+// getIcon reads an icon file from the given path.
+func getIcon(filePath string) []byte {
+	icon, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Fatalf("Error during downloading icon: %v", err)
+	}
+	return icon
 }
